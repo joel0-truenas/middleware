549f704bf8e1cb2f7e45028bc78df88ee67f7e53
Joel May <git@jmay.us>
2025-04-30 20:46:52 -0700

    Add support for aarch64

--- src/middlewared/middlewared/utils/nss/nss_common.py
+++ src/middlewared/middlewared/utils/nss/nss_common.py
@@ -1,8 +1,10 @@
 import enum
 import ctypes
 import os
+import platform
 
-NSS_MODULES_DIR = '/usr/lib/x86_64-linux-gnu'
+ARCH = platform.machine()
+NSS_MODULES_DIR = f'/usr/lib/{ARCH}-linux-gnu'
 FILES_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_files.so.2')
 SSS_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_sss.so.2')
 WINBIND_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_winbind.so.2')
